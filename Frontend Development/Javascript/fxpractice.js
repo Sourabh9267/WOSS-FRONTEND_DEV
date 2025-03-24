d9c036c9dad80c0a20e1ee062134b6e711106721
Sourabh Gour <saurabhgour2222@gmail.com>
2025-03-24 18:06:00 +0530

    JS Practice Code added

---
+++ Frontend Development/Javascript/fxpractice.js
@@ -0,0 +1,40 @@
+// function mainfx(message){
+
+//     return function secondary(user="Default"){
+//         console.log(`Value of MainFx is : ${message} and Value of secondary fx is : ${user}`);
+//     }
+
+// }
+
+// const result = mainfx("Hi from main");
+
+
+// console.log(result);
+// console.log(result());
+// console.log(result("User"));
+
+
+// result("by from secondary");
+
+
+
+// arr = ["sourabh","pranjal","suraj"];
+
+// console.log(arr.map(name=>"Hello" + name));
+
+
+// function outer(){
+//     let outerVariable="I Am Outer Variable";
+//     console.log("reached in outer");
+//     function inner(){   
+//         console.log("reached in inner");
+//         console.log(outerVariable);
+//     }
+//     console.log("left outer");
+//     return inner;
+// }
+
+
+// let closureFunc=outer();
+// // closureFunc();
+
