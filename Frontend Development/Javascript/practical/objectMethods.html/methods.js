8c34d8f843bd8c42ed19e14c7a65aef9b2d0dce6
Sourabh Gour <saurabhgour2222@gmail.com>
2025-03-27 17:58:42 +0530

    Global This Keyword etc practice files are now added

---
+++ Frontend Development/Javascript/practical/objectMethods.html/methods.js
@@ -0,0 +1,17 @@
+const person = {
+    firstName : "John",
+    lastName  : "Doe",
+    age     : 50,
+    };
+
+let arr=[new Object(person),new Object(person),new Object(person)]
+console.log(arr);
+
+
+
+    let text = Object.entries(person);
+
+    // console.log(text);
+    
+    
+    
