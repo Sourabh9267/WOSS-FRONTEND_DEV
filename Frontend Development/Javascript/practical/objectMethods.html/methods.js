ae6ae0ccc73b28e8769638b9c8069c0ecc0dffb7
Sourabh Gour <saurabhgour2222@gmail.com>
2025-03-28 15:34:03 +0530

    JS Practice files added

--- Frontend Development/Javascript/practical/objectMethods.html/methods.js
+++ Frontend Development/Javascript/practical/objectMethods.html/methods.js
@@ -1,17 +1,13 @@
-const person = {
-    firstName : "John",
-    lastName  : "Doe",
-    age     : 50,
-    };
+// let str= "Hello";
 
-let arr=[new Object(person),new Object(person),new Object(person)]
+// let strM= str.split("");
+
+// let setX=new Set(strM);
+
+// console.log(setX);
+
+let setX = new Set([1,2,3]);
+
+let arr=[...setX];
+
 console.log(arr);
-
-
-
-    let text = Object.entries(person);
-
-    // console.log(text);
-    
-    
-    
